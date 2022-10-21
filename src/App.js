8c254cb039d6166cf50cb1d5bc30ad784b2499a3
Dinar Davis <dinardavis@gmail.com>
2022-10-21 14:34:37 +0300

    Refactor code to move all functionality to App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
 import React from 'react'
-import Header from './Header'
+import { BsTrash } from "react-icons/bs";
 
 export default function App() {
+  const [listData, setListData] = React.useState(["first thing", "second thing", "next thing"])
+  const [userInput, setUserInput] = React.useState("")
 
+  function handleChange(e) {
+    setUserInput(e.target.value)
+  }
+
+  console.log(userInput)
+
+  function addListData() {
+    setListData(prevListData => {
+      return [...prevListData, userInput]
+    })
+    document.querySelector('.todo-input').value = ""
+  }
+  
+  let renderedList = listData.map((item, index) => {
+    return (
+      <div className='list-item'
+        key={index}
+        id={index}
+      >
+        <div className='category-marker'></div>
+        {item}
+        <BsTrash className='trash-icon'/>
+      </div>
+    )
+  })
+  
   return (
     <section className='main-container'>
-      <Header />
+      <div className='header-container'>
+        <input 
+          className='todo-input'
+          type="text"
+          placeholder="Add To-do"
+          onChange={handleChange}
+        />
+        <button 
+          className='add-btn'
+          onClick={addListData}
+        >+</button>
+      </div>
+  
+      {renderedList}
     </section>
   )
 }
